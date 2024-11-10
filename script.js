1f21c07c7e00356a864bc69b56589485c9da60a7
axelquartz <axelarep@gmail.com>
2024-11-10 17:00:04 -0600

    Script started

---
+++ script.js
@@ -0,0 +1,24 @@
+// Screen container
+let screen = document.getElementById("screen");
+
+// Buttons mapping
+let keySeven = document.querySelector(".seven");
+// const key8 = document.querySelector(".8");
+// const key9 = document.querySelector(".9");
+// const keyDiv = document.querySelector(".div");
+// const key4 = document.querySelector(".4");
+// const key5 = document.querySelector(".5");
+// const key6 = document.querySelector(".6");
+// const keyMultiply = document.querySelector(".multiply");
+// const key1 = document.querySelector(".1");
+// const key2 = document.querySelector(".2");
+// const key3 = document.querySelector(".3");
+// const keySubtract = document.querySelector(".subtract");
+// const key0 = document.querySelector(".0");
+// const keyDecimal = document.querySelector(".decimal");
+// const keyAdd = document.querySelector(".add");
+// const keyEqual = document.querySelector(".equal");
+
+keySeven.addEventListener("click", () => {
+  screen.innerHTML += "7";
+});
